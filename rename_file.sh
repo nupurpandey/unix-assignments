c5e5574a77e3947e6616fdcecfefbf3a68ca0cef
Nupur Pandey <mail.nupurpandey@gmail.com>
2019-07-13 13:33:14 +0200

    Added more files of the assignment

--- rename_file.sh
+++ rename_file.sh
@@ -5,12 +5,18 @@
 echo "Enter file with sh extension: "
 read file
 
-if [[ -e $file ]]; then
+if [[ $file == *.sh ]]; then
+  if [[ -e $file ]]; then
     if [[ -f $file ]]; then
+        echo "Now changing the extension of file"
         mv "$file" "${file%sh}exe"
     else
        echo "This is not a regular file"
     fi
+  else
+     echo "This file do no exists"
+  fi
 else
-    echo "This file do no exists"
+   echo "I told you to provide file with sh extension, Exiting the program"
+   exit 1
 fi
